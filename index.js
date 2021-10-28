269e6711c2d662f7ecee20247b2f47aec0f46b37
zhuangjinan <zhuangjinan2018@gmail.com>
2021-10-28 17:18:44 +0800

    3.8

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
 
+morgan.token('request-body', (req, _) => {
+    return  req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens['request-body'](req, res),
+    ].join(' ')
+}))
 
 let persons = [
     {
